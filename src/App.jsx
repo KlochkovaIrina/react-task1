df93512db36cef13e21d8f2e41ed7eb2948b6cd7
KlochkovaIrina <109291169+KlochkovaIrina@users.noreply.github.com>
2025-02-17 18:51:19 +0300

    component without jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 
-const currentYear = new Date().getFullYear();//декларативный подход
+// const currentYear = new Date().getFullYear();//декларативный подход
 
 
-export const App = ()=>{
-	return (
-		//декларативный подход
-		<div className='App'>
-			<footer className="App-footer">
-				<a href="#" className="App-link">Learn React</a>
-				<h1 className='currentYear'>{currentYear}</h1>
-			</footer>
-		</div>
-	)
-}
+// export const App = ()=>{
+// 	return (
+// 		//декларативный подход
+// 		<div className='App'>
+// 			<footer className="App-footer">
+// 				<a href="#" className="App-link">Learn React</a>
+// 				<h1 className='currentYear'>{currentYear}</h1>
+// 			</footer>
+// 		</div>
+// 	)
+// }
+import React from 'react';
 
+const currentYear = new Date().getFullYear(); // Декларативный подход
 
-
-
+export const App = () => {
+  return React.createElement(
+    'div',
+    { className: 'App' },
+    React.createElement(
+      'footer',
+      { className: 'App-footer' },
+      React.createElement(
+        'a',
+        { href: '#', className: 'App-link' },
+        'Learn React'
+      ),
+      React.createElement(
+        'h1',
+        { className: 'currentYear' },
+        currentYear
+      )
+    )
+  );
+};
 
 
