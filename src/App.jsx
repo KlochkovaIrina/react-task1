78cbaa919487a4229c5294715810faf7301e99c9
KlochkovaIrina <109291169+KlochkovaIrina@users.noreply.github.com>
2025-02-17 17:53:01 +0300

    with jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+const currentYear = new Date().getFullYear();//декларативный подход
+
+export const App = ()=>{
+	return (
+		//декларативный подход
+		<div className='App'>
+			<footer className="App-footer">
+				<a href="#" className="App-link">Learn React</a>
+				<h1 className='currentYear'>{currentYear}</h1>
+			</footer>
+		</div>
+	)
+}
+
+
+
+
+
+
